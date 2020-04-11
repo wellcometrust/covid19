4d962015817b40cb1e688be4f65a921e6ca8370b
Nick Sorros <nsorros@gmail.com>
2020-04-11 17:56:57 +0300

    Add wasabi for better printing

--- covid/question_covid.py
+++ covid/question_covid.py
@@ -1,8 +1,7 @@
-from transformers import BertTokenizer, BertForQuestionAnswering
-
+from transformers import BertTokenizer, BertForQuestionAnswering, pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
-
+from wasabi import msg, table, row
 import pandas as pd
 import torch
 
@@ -25,18 +24,19 @@ class QuestionCovid:
         self.MODEL = MODEL
         self.index2paperID = index2paperID
         self.index2paperPath = index2paperPath
+        self.nlp = pipeline("question-answering")
 
     def fit(self, data_text):
 
         self.TFIDF_VECTORIZER = TfidfVectorizer()
-        print("   Fitting TFIDF", end=" ", flush=True)
-        start = time.time()
-        self.TFIDF_VECTORIZER.fit(data_text.values())
-        print(" - Took {:.2f}s".format(time.time()-start))
-        print("   Creating Articles matrix", end="", flush=True)
-        start = time.time()
-        self.ARTICLES_MATRIX = self.TFIDF_VECTORIZER.transform(data_text.values())
-        print(" - Took {:.2f}s".format(time.time()-start))
+        with msg.loading("   Fitting TFIDF"):
+            start = time.time()
+            self.TFIDF_VECTORIZER.fit(data_text.values())
+        msg.good("   TFIDF fitted - Took {:.2f}s".format(time.time()-start))
+        with msg.loading("   Creating Articles matrix"):
+            start = time.time()
+            self.ARTICLES_MATRIX = self.TFIDF_VECTORIZER.transform(data_text.values())
+        msg.good("   Article matrix created - Took {:.2f}s".format(time.time()-start))
 
     def get_answer(self, text, question):
 
@@ -46,21 +46,23 @@ class QuestionCovid:
         start_scores, end_scores = self.MODEL(torch.tensor([input_ids]), token_type_ids=torch.tensor([token_type_ids]))
         all_tokens = self.TOKENIZER.convert_ids_to_tokens(input_ids)
         answer = ' '.join(all_tokens[torch.argmax(start_scores) : torch.argmax(end_scores)+1])
+        score = round(start_scores.max().item(), 2)
+        #response = self.nlp({'question': question, 'context': text})
+        #answer = response["answer"]
+        #score = response["score"]
 
-        return answer, start_scores.max(), end_scores.max()
+        return answer, score
 
     def predict(self, question):
 
         query = self.TFIDF_VECTORIZER.transform([question])
         best_matches = sorted([(i,c) for i, c in enumerate(cosine_similarity(query, self.ARTICLES_MATRIX).ravel())], key=lambda x: x[1], reverse=True)
 
-        best_answers = []
         for i, _ in best_matches[:5]:
             best_score = 0 # if score is negative, i consider the answer wrong
             best_answer = None
             best_text = None
-
-            print(self.index2paperID[i])
+            
             paper_path = self.index2paperPath[i]
             with open(paper_path) as json_file:
                 article_data = json.load(json_file)
@@ -68,13 +70,12 @@ class QuestionCovid:
 
             for text_i in range(0, len(text), 512):
                 subtext = text[text_i:text_i+512]
-                answer, start_score, end_score = self.get_answer(subtext, question)
-                if start_score > best_score:
-                    best_score = start_score
+                answer, score = self.get_answer(subtext, question)
+                if score > best_score:
+                    best_score = score
                     best_answer = answer
                     best_text = subtext
-            best_answers.append((self.index2paperID[i], best_text, best_answer, best_score))
-        return best_answers
+            yield (self.index2paperID[i], best_answer, best_score, best_text)
 
 def get_data_texts(articles_dir, articles_folders):
 
@@ -96,9 +97,10 @@ def get_data_texts(articles_dir, articles_folders):
         return data_text, index2paperID, index2paperPath
 
 def train():
-    print("   Loading BERT")
-    TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
-    MODEL = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    with msg.loading("   Loading BERT"):
+        TOKENIZER = BertTokenizer.from_pretrained('bert-base-uncased')
+        MODEL = BertForQuestionAnswering.from_pretrained('bert-large-uncased-whole-word-masking-finetuned-squad')
+    msg.good("   BERT loaded")
 
     articles_dir = 'data/raw/CORD-19-research-challenge/'
     articles_folders = [
@@ -110,10 +112,10 @@ def train():
         'custom_license/custom_license/pdf_json/',
         'custom_license/custom_license/pmc_json/']
 
-    print("   Ingesting publications", end=" ", flush=True)
-    start = time.time()
-    data_text, index2paperID, index2paperPath = get_data_texts(articles_dir, articles_folders)
-    print("- Took {:.2f}s".format(time.time()-start))
+    with msg.loading("   Loading publications"):
+        start = time.time()
+        data_text, index2paperID, index2paperPath = get_data_texts(articles_dir, articles_folders)
+    msg.good("   Publications loaded - Took {:.2f}s".format(time.time()-start))
 
     covid_q = QuestionCovid(TOKENIZER, MODEL, index2paperID, index2paperPath)
     covid_q.fit(data_text)
@@ -131,6 +133,11 @@ def create_argparser():
         '--pre_trained_model_path',
         help='Path to pretrained covid q model'
     )
+    parser.add_argument(
+        '--answers_path',
+        help='Path to save answers',
+        default='answers.jsonl'
+    )
 
     return parser
 
@@ -140,21 +147,36 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     if args.pre_trained_model_path:
-        print("Loading pre trained model")
-        with open(args.pre_trained_model_path, "rb") as f:
-            covid_q = pickle.load(f)
+        with msg.loading("Loading pre trained model"):
+            with open(args.pre_trained_model_path, "rb") as f:
+                covid_q = pickle.load(f)
+        msg.good(f"Loaded {args.pre_trained_model_path}")
     else:
-        print("Training new model")
+        msg.text("Training new model")
         covid_q = train()
-        print("   Saving model")
+        msg.good("Trained")
         with open("models/covid_q.pkl", "wb") as f:
             pickle.dump(covid_q, f)
 
+
     print("Finding best answer")
     start = time.time()
-    best_answers = covid_q.predict(args.question)
+    with open(args.answers_path, "w") as f:
+        header = ["Paper id", "Answer", "Score", "Snippet"]
+        widths = (40, 40, 5, 35)
+        print(table((), header=header, divider=True, widths=widths))
+        for paper_id, answer, score, snippet in covid_q.predict(args.question):
+            data = (paper_id, answer, score, snippet[:15] + '...' + snippet[-15:])
+            print(row(data, widths=widths))
+            chunk = json.dumps({
+                'paper_id': paper_id,
+                'answer': answer,
+                'snippet': snippet,
+                'score': score
+            })
+            f.write(chunk + '\n')
 
-    print("----- Answer: -----")
-    for best_answer in best_answers:
-        print(best_answer)
-    print(" - Took {}s".format(time.time()-start))
+
+        # save to file for inspection
+
+    print("\nTook {}s".format(time.time()-start))
